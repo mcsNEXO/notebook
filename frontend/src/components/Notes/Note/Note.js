deb445ab522e4b875cee761e72959d7ded760635
Jakub Kowalski <mcsnexo@gmail.com>
2022-05-24 20:32:51 +0200

    Update notes,note

--- frontend/src/components/Notes/Note/Note.js
+++ frontend/src/components/Notes/Note/Note.js
@@ -2,6 +2,15 @@ import './Note.css'
 
 export default function Note(props) {
     return (
-        <div>Note</div>
+        <div className="container-note">
+            <div className='title-note'>
+                Title
+            </div>
+            <div className='note-line'><hr className='line'></hr><button>Show Note</button><hr className='line'></hr></div>
+            <div className='description'>
+                Description:
+                Description
+            </div>
+        </div >
     )
 };
--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,5 +1,18 @@
+import './Notes.css'
+import Note from './Note/Note'
+
 export default function Notes(props) {
     return (
-        <div>Notes</div>
+        <>
+            <button className='add-note'>Add Note</button>
+            <div className='container'>
+                <h3 className='title'>Twoje notatki:</h3>
+                <div className=''>
+                    <div className='notes'>
+                        <Note />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 };
